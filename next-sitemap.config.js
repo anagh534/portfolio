6f0bf764cc662eb719d9660280b53310dcf4b2fd
anagh534 <anaghkrkkl@gmail.com>
2024-12-23 13:45:12 +0530

    some changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,18 @@
 // next-sitemap.config.js
 
 module.exports = {
-    siteUrl: 'https://anagh534.github.io', // Replace with your actual domain
-    generateRobotsTxt: true, // (optional)
-    exclude: ['/admin/**', '/404'], // Optional: Pages you want to exclude from the sitemap
-    robotsTxtOptions: {
-      policies: [
-        { userAgent: '*', allow: '/' }, // Allow all bots to crawl all pages
-      ],
-    },
-  };
-  
+  siteUrl: 'https://anagh534.github.io', // Replace with your actual domain
+  generateRobotsTxt: true, // (optional)
+  exclude: ['/admin/**', '/404'], // Optional: Pages you want to exclude from the sitemap
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' }, // Allow all bots to crawl all pages
+    ],
+  },
+  additionalPaths: async (config) => {
+    return [
+      { loc: '/about', changefreq: 'weekly', priority: 0.8 },
+      { loc: '/services', changefreq: 'weekly', priority: 0.9 },
+    ];
+  },
+};
