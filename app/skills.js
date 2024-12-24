19193785dcb026d2ce3b23225cce30d138c9b78a
anagh534 <anaghkrkkl@gmail.com>
2024-12-24 19:21:40 +0530

    skill section updated

--- app/skills.js
+++ app/skills.js
@@ -8,7 +8,7 @@ export default function Skills() {
                 </div>
                 <div className="row text-center pb-5">
                     <h2>Skills</h2>
-                    <p>I am striving to never stop learning and improving</p>
+                    <p>As a passionate developer, I am dedicated to honing my skills and staying updated with the latest technologies.<br /> My expertise spans across web development, mobile app development, and SEO optimization, ensuring that I deliver cutting-edge solutions tailored to your needs.</p>
                 </div>
                 <div className="cards">
                     <div className="row">
@@ -16,8 +16,11 @@ export default function Skills() {
                             <div className="card mb-3" style={{ minHeight: '137px' }}>
                                 <div className="card-body text-center">
                                     <i className="fa-solid fa-desktop"></i>
-                                    <h5 className="card-title">WEB DEVELOPMENT</h5>
-                                    <p className="card-text"><small className="text-body-secondary"><i className="fa-brands fa-beat  fa-html5"></i> HTML <i className="fa-brands fa-beat  fa-css3-alt"></i> CSS <i className="fa-brands fa-beat  fa-js"></i> JS <br /> <i className="fa-brands fa-beat  fa-node"></i> MERN</small></p>
+                                    <h4 className="card-title">WEBSITE DEVELOPMENT</h4>
+                                    <p className="card-text"><small className="text-body-secondary">
+                                        <i className="fa-solid fa-code"></i> Custom Website Solutions <br />
+                                        <i className="fa-solid fa-mobile-alt"></i> Responsive Web Design <br />
+                                        <i className="fa-solid fa-cogs"></i> Website Optimization & Maintenance</small></p>
                                 </div>
                             </div>
                         </div>
@@ -25,8 +28,9 @@ export default function Skills() {
                             <div className="card mb-3" style={{ minHeight: '137px' }}>
                                 <div className="card-body text-center">
                                     <i className="fa-solid fa-mobile-screen-button"></i>
-                                    <h5 className="card-title">APP DEVELOPMENT</h5>
-                                    <p className="card-text"><small className="text-body-secondary"><i className="fa-brands fa-flutter fa-beat "></i> FLUTTER</small></p>
+                                    <h4 className="card-title">APP DEVELOPMENT</h4>
+                                    <p className="card-text"><small className="text-body-secondary"><i className="fa-brands fa-flutter "></i> Flutter (Cross-platform apps for Android and iOS)<br/><i className="fa-solid fa-cloud"></i> Real-Time Database Integration <br/>
+                                    <i className="fa-solid fa-bell"></i> Push Notifications & Authentication</small></p>
                                 </div>
                             </div>
                         </div>
@@ -34,8 +38,8 @@ export default function Skills() {
                             <div className="card mb-3" style={{ minHeight: '137px' }}>
                                 <div className="card-body text-center">
                                     <i className="fa-solid fa-desktop"></i>
-                                    <h5 className="card-title">SEO Optimization</h5>
-                                    <p className="card-text"><small className="text-body-secondary"><i className="fa-brands fa-searchengin fa-beat"></i> SEO</small></p>
+                                    <h4 className="card-title">SEO Optimization</h4>
+                                    <p className="card-text"><small className="text-body-secondary"><i className="fa-brands fa-searchengin"></i> On-page and technical SEO to boost search rankings</small></p>
                                 </div>
                             </div>
                         </div>
@@ -43,11 +47,72 @@ export default function Skills() {
                             <div className="card mb-3" style={{ minHeight: '137px' }}>
                                 <div className="card-body text-center">
                                     <i className="fa-solid fa-desktop"></i>
-                                    <h5 className="card-title">WEB DESIGN</h5>
-                                    <p className="card-text"><small className="text-body-secondary"><i className="fa-brands fa-figma fa-beat"></i> FIGMA <i className="fa-solid fa-square-parking fa-beat"></i> PHOTOSHOP</small></p>
+                                    <h4 className="card-title">WEB DESIGN</h4>
+                                    <p className="card-text"><small className="text-body-secondary"><i className="fa-brands fa-figma"></i> Figma to Website Conversion <br /><i className="fa-solid fa-square-parking "></i> PHOTOSHOP (.PSD) to Responsive Website</small></p>
                                 </div>
                             </div>
                         </div>
+                        <div className="col-lg-6">
+                            <div className="card mb-3" style={{ minHeight: '137px' }}>
+                                <div className="card-body text-center">
+                                    <i className="fa-brands fa-wordpress"></i>
+                                    <h4 className="card-title">WORDPRESS DEVELOPMENT</h4>
+                                    <p className="card-text">
+                                        <small className="text-body-secondary">
+                                            <i className="fa-solid fa-lock"></i> WordPress Security Check-Up <br />
+                                            <i className="fa-solid fa-wrench"></i> Custom WordPress Development <br />
+                                            <i className="fa-solid fa-paintbrush"></i> WordPress Website Redesign
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="card mb-3" style={{ minHeight: '137px' }}>
+                                <div className="card-body text-center">
+                                    <i className="fa-solid fa-store"></i>
+                                    <h4 className="card-title">E-COMMERCE DEVELOPMENT</h4>
+                                    <p className="card-text">
+                                        <small className="text-body-secondary">
+                                            <i className="fa-solid fa-cart-arrow-down"></i> Custom E-commerce Solutions <br />
+                                            <i className="fa-solid fa-credit-card"></i> Secure Payment Integration <br />
+                                            <i className="fa-solid fa-gift"></i> User-friendly Product Listings
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="card mb-3" style={{ minHeight: '137px' }}>
+                                <div className="card-body text-center">
+                                    <i className="fa-brands fa-react"></i>
+                                    <h4 className="card-title">NEXT.JS DEVELOPMENT</h4>
+                                    <p className="card-text">
+                                        <small className="text-body-secondary">
+                                            <i className="fa-solid fa-cogs"></i> Server-Side Rendering (SSR) <br />
+                                            <i className="fa-solid fa-laptop-code"></i> Static Site Generation (SSG) <br />
+                                            <i className="fa-solid fa-rocket"></i> Performance Optimization
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="card mb-3" style={{ minHeight: '137px' }}>
+                                <div className="card-body text-center">
+                                    <i className="fa-brands fa-node"></i>
+                                    <h4 className="card-title">MERN STACK DEVELOPMENT</h4>
+                                    <p className="card-text">
+                                        <small className="text-body-secondary">
+                                            <i className="fa-brands fa-react"></i> React.js for Dynamic UIs <br />
+                                            <i className="fa-brands fa-node"></i> Node.js & Express.js Backend <br />
+                                            <i className="fa-solid fa-database"></i> MongoDB for Scalable Data Solutions
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
