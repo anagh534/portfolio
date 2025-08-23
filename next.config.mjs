2b476897195d957b1c010656156ae1d4f4248ff4
anagh534 <anaghkrkkl@gmail.com>
2025-08-24 00:40:52 +0530

    next config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",   // 👈 required
+    images: { unoptimized: true },
     async redirects() {
         return [
             {
