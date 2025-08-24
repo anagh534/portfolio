02a47ad30045265dfc5d8f01a9f97656b35b5bb4
anagh534 <anaghkrkkl@gmail.com>
2025-08-24 09:39:45 +0530

    cdn change to npm package

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer/footer";
@@ -6,25 +8,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* SEO and external stylesheets */}
+        {/* SEO and meta tags */}
         <meta name="google-site-verification" content="AWxjk_Fpn8FzilJKbuxl3i1J8VFgrfVwEkoaqwKp9mo" />
         <meta charSet="utf-8"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1"/>
-          <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
-            integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
-          <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml" />
-          <link rel="sitemap" type="application/xml" title="Sitemap-0" href="/sitemap-0.xml" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml" />
+        <link rel="sitemap" type="application/xml" title="Sitemap-0" href="/sitemap-0.xml" />
       </head>
 
       <body>
@@ -32,13 +21,6 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
 
-        {/* Bootstrap JS Bundle */}
-        <script
-          rel="preload"
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-          crossOrigin="anonymous"
-        ></script>
         {/* ✅ Structured Data for Google */}
         <script
           type="application/ld+json"
