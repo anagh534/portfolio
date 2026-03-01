6a3411caf12156d1b9be39e2ef0b0a8aaeb82c01
anagh534 <anaghkrkkl@gmail.com>
2026-03-01 10:42:33 +0530

    feat: Update font settings in globals.css and layout.js for improved consistency and performance

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,6 @@
 import "./globals.css";
 import ClientLayout from "./ClientLayout";
 import { metadata as meta, viewport as view } from "./metadata";
-import { Outfit, Plus_Jakarta_Sans } from "next/font/google";
-
-const outfit = Outfit({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-outfit",
-  weight: ["100", "300", "400", "500", "600", "700", "800"],
-});
-
-const plusJakartaSans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-jakarta",
-  weight: ["200", "300", "400", "500", "600", "700", "800"],
-});
 
 export const metadata = meta;
 export const viewport = view;
@@ -23,7 +8,7 @@ export const viewport = view;
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${outfit.variable} ${plusJakartaSans.variable}`}>
+      <body>
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
